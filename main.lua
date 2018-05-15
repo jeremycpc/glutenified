0366c1ac175da7d6f160e525e4b9caa02629a1d9
Jeremy Chiang <jeremycpc2@gmail.com>
2018-05-16 01:05:04 +1000

    Added images:

---
+++ main.lua
@@ -0,0 +1,24 @@
+push = require 'push'
+
+Class = require 'class'
+
+require 'StateMachine'
+
+require 'states/BaseState'
+require 'states/CountdownState'
+require 'states/PlayState'
+require 'states/ScoreState'
+require 'states/TitleScreenState'
+
+require 'Bird'
+require 'Pipe'
+require 'PipePair'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
